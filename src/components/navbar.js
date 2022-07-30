23e8035130498ce30d0c464001f5f269f868e017
Aymenob <aymenobba@gmail.com>
2022-07-31 01:53:01 +0200

    Todolist

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Navbar({updatesection1,updatesection2,updatesection3}) {
   return (
-    <div style={{display:"flex",justifyContent:'space-evenly'}}>
+    <div  style={{display:"flex",justifyContent:'space-evenly'}}>
       <a  onClick={()=>updatesection1()} style={{textDecoration:'none',color:"white",fontSize:"21px",fontWeight:"500"}} href='#'>All</a>
       <a name='Done' onClick={()=>updatesection2()} style={{textDecoration:'none',color:"white",fontSize:"21px",fontWeight:"500"}} href='#'>Done</a>
       <a name="Undone" onClick={()=>updatesection3()} style={{textDecoration:'none',color:"white",fontSize:"21px",fontWeight:"500"}}  href='#'>Undone</a>
